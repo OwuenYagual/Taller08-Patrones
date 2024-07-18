f20889d545438a3be4decbaa839d6be4883606fd
Javier Rodriguez <javier28420@gmail.com>
2024-07-17 22:48:59 -0500

    Update Customer.java

--- RetailStore/src/main/java/com/mycompany/retailstore/Customer.java
+++ RetailStore/src/main/java/com/mycompany/retailstore/Customer.java
@@ -1,12 +1,17 @@
 package com.mycompany.retailstore;
 
+import ec.edu.espol.strategypattern.NotificarStrategy;
+
 public class Customer implements Observer{
     private String email;
+    private NotificarStrategy notificationStrategy;
 
     public Customer(String email) {
         this.email = email;
     }
-    
+    public void setNotificationStrategy(NotificarStrategy notificationStrategy) {
+        this.notificationStrategy = notificationStrategy;
+    }
     
     @Override
     public void update(String message) {

8ae001901f78c0eefa7d2e6e985d377d8d17e2da
Owen Yagual Panimboza <oayagual@espol.edu.ec>
2024-07-17 23:01:50 -0500

    creacion de paquete para strategy pattern

---
+++ RetailStore/src/main/java/Strategy/FacebookStrategy.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.strategypattern;
+
+/**
+ *
+ * @author Usuario
+ */
+public class FacebookStrategy {
+    //informacion del cliente
+    
+    public void notificar(){
+        // Procedimientos necesarios para enviar notificaciones por Facebook
+        System.out.println("Enviar notificación por Facebook");
+
+    }
+    
+}
---
+++ RetailStore/src/main/java/Strategy/NotificarCliente.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.strategypattern;
+
+/**
+ *
+ * @author Usuario
+ */
+public class NotificarCliente {
+    private NotificarStrategy strategy;
+    
+    
+    public void setStrategy(NotificarStrategy ss){
+        //Configurar la estrategia que vamos a usar
+    }
+    
+    public void NotificarCliente(){
+        //Codigo necesario para enviar notificaciones a los clientes
+    }
+}
